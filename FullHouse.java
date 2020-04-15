b667b8268d2324594a6c6109d762f369ee9d703c
HariniNallangilli <62899531+HariniNallangilli@users.noreply.github.com>
2020-04-15 16:02:17 -0400

    Interface-W3D1

---
+++ FullHouse.java
@@ -0,0 +1,25 @@
+package week3.day3;
+
+
+	public class FullHouse implements HouseDesign {
+
+		public void chooseColor(String color) {
+			System.out.println("Yellowish Gray");
+		}
+
+		public void designBasement() {
+			System.out.println("Done");		
+		}
+
+		public int getNumberOfFloors() {
+			return 2;
+		}
+
+		/*
+		 * can have your own methods in addition what interface told you
+		 */
+		public void carParking() {
+			System.out.println("Implement");
+		}
+		
+	}
