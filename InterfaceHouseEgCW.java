26780cec48c0f29e252c873ce4d1e0b31ff9ea5f
HariniNallangilli <62899531+HariniNallangilli@users.noreply.github.com>
2020-04-15 15:47:57 -0400

    W3D1 Implements CW

---
+++ InterfaceHouseEgCW.java
@@ -0,0 +1,20 @@
+package week3.day3;
+
+public class InterfaceHouseEgCW {
+	public void chooseColor(String color) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void designBasement() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getNumberOfFloors() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+}
